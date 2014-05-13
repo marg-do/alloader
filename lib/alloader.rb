6e8f9b431bc251f1414face85a346e6011e9aa25
marg_do <t.t.mc1192.sf@gmail.com>
2014-05-14 07:18:45 +0900

    0.0.2

--- lib/alloader.rb
+++ lib/alloader.rb
@@ -1,39 +1,47 @@
-#require "alloader/version"
+require "alloader/version"
 
 module Alloader
 
-  def required_from; $0 end
+  class << self
 
-  def is_directory(path)
-    FileTest::directory?(path)
-  end
-
-  def filelist_in(dir_path)
-    list = (Dir::entries(dir_path) - [".", "..", required_from]).map {|path|
-      File::expand_path(dir_path) + "/" + path
-	  }
-    dirs = list.select { |x| FileTest::directory?(x) }
-    files = list - dirs
-    files_in_dirs = dirs.inject([]) { |acc, dir| acc + filelist_in(dir) }
-
-    files + files_in_dirs
-  end
-
-  def require_dir(dirpath, mode = :require)
-    case mode
-    when :require
-      filelist_in(dirpath).each do |path|
-        require path
-      end
-    when :load
-      filelist_in(dirpath).each do |path|
-        load path
+    def require_all(dirpath, mode = :require)
+      case mode
+      when :require
+        filelist_in(dirpath).each do |f|
+          require f
+        end
+      when :load
+        filelist(dirpath).each do |f|
+          load f
+        end
       end
     end
+
+    def r
+      load caller_head
+    end
+
+    private
+
+    def caller_files
+      caller.map { |str| File::expand_path(str.split(":")[0]) }
+    end
+
+    def caller_head
+      caller_files[-1]
+    end
+
+    def filelist_in(dir_path)
+      list = (Dir::entries(dir_path) - [".", "..", $0]).map {|path|
+        File::expand_path(dir_path) + "/" + path
+	    } - caller_files
+      dirs = list.select { |x| FileTest::directory?(x) }
+      files = list - dirs
+      files_in_dirs = dirs.inject([]) { |acc, dir| acc + filelist_in(dir) }
+
+      files + files_in_dirs
+    end
+
   end
-
-  def r; load required_from end
-
-  module_function :require_dir, :r
 
 end
