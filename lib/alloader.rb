9ceab51d7d6fff497c7b5982149e0dcf08e471f0
marg_do <t.t.mc1192.sf@gmail.com>
2014-05-14 08:01:09 +0900

    0.0.22 <2>

--- lib/alloader.rb
+++ lib/alloader.rb
@@ -4,42 +4,54 @@ module Alloader
 
   class << self
 
-    def require_all(dirpath, mode = :require)
-      case mode
-      when :require
-        filelist_in(dirpath).each do |f|
-          require f
-        end
-      when :load
-        filelist_in(dirpath).each do |f|
-          load f
-        end
-      end
+    def load_dir(dirpath)
+      load_filepaths(filepaths_in_dir(dirpath))
     end
 
-    def r
-      load caller_head
+    def require_dir(dirpath)
+      require_filepaths(filepaths_in_dir(dirpath))
     end
 
     private
 
-    def caller_files
-      caller.map { |str| File::expand_path(str.split(":")[0]) }
+    # ----------------------------------------
+
+    def is_dir(path)
+      FileTest::directory?(path)
     end
 
-    def caller_head
-      caller_files[-1]
+    def filepaths_in_dir(dirpath)
+      filelist = (Dir::entries(dirpath) - [".", ".."]).map { |file|
+        File::expand_path(dirpath + "/" + file)
+      }
+      dirpaths = filter(method(:is_dir), filelist)
+      filepaths_in_dirpaths = dirpaths.inject([]) { |acc, dirpath|
+        acc + filepaths_in_dir(dirpath)
+      }
+
+      filelist - dirpaths + filepaths_in_dirpaths
     end
 
-    def filelist_in(dir_path)
-      list = (Dir::entries(dir_path) - [".", "..", $0]).map {|path|
-        File::expand_path(dir_path) + "/" + path
-	    } - caller_files
-      dirs = list.select { |x| FileTest::directory?(x) }
-      files = list - dirs
-      files_in_dirs = dirs.inject([]) { |acc, dir| acc + filelist_in(dir) }
+    def require_filepaths(filepaths)
+      result = {}
+      filepaths.each do |filepath|
+        result[filepath] = require filepath
+      end
+      result
+    end
 
-      files + files_in_dirs
+    def load_filepaths(filepaths)
+      result = {}
+      filepaths.each do |filepath|
+        result[filepath] = load filepath
+      end
+      result
+    end
+
+    # ----------------------------------------
+
+    def filter(pred, lst)
+      lst.select { |itm| pred.(itm) }
     end
 
   end
